62b8569e93ba6e039ddb7cfc1dd6e114ca1485a1
Daniel <schnolldj@gmail.com>
2018-04-26 16:45:36 -0400

    Implemented a login page and respective php

--- sql/users.sql
+++ sql/users.sql
@@ -3,14 +3,7 @@
 CREATE TABLE `users` (
  `id` int unsigned NOT NULL AUTO_INCREMENT,
  `username` varchar(100) NOT NULL,
+ `email` varchar(100) NOT NULL,
+ `password` varchar(100) NOT NULL,
  PRIMARY KEY (`id`)
 );
-
-
-INSERT INTO `users` VALUES
-	(1, "Daniel"),
-	(2, "Justin"),
-	(3, "Mark"),
-	(4, "Sally"),
-	(5, "baseballpro56"),
-	(6, "Professor Plotka");
